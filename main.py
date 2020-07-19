a5984403a1efa5af393cf329de658da97da7ab34
Jeremy Berman <jerber@sas.upenn.edu>
2020-07-19 19:30:15 -0400

    took out craete handler... not needed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from magicapi import app, router, create_handler
+from magicapi import app, router, handler
 
 # add imports here
 from app import Routes
 
 app.include_router(router)
-handler = create_handler(app)
