cbe7685296fff544ca93bffe582dd117d685c69a
Jeremy Berman <jerber@sas.upenn.edu>
2020-07-23 21:53:05 -0400

    now tested w errors and everything

--- app/config.py
+++ app/config.py
@@ -1,10 +1,13 @@
-from magicapi.config import MagicSettings
+from magicapi.config import MagicSettings, find_file
 
 
 class CustomSettings(MagicSettings):
     custom_settings: bool = True
 
     app_name: str = "sploopery"
+
+    class Config:
+        env_file = find_file(glob_string="*.env") or ".env"
 
 
 custom_settings = CustomSettings()
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from app.config import custom_settings
 
 from magicapi import create_app, create_handler
 
+custom_settings.app_name = custom_settings.service
+
 app = create_app(config_settings=custom_settings)
 
 # add imports here...
